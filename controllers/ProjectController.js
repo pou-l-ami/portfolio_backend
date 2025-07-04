80fab68e1ffa26059d284499715faca5b9ebda29
pou-l-ami <poumaliacharya22@gmail.com>
2025-07-04 15:50:32 +0530

    Delete and Update endpoint added

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,5 +1,6 @@
 const Project = require('../models/Project');
 
+// GET all projects
 const getProjects = async (req, res) => {
   try {
     const projects = await Project.find();
@@ -9,6 +10,7 @@ const getProjects = async (req, res) => {
   }
 };
 
+// POST new project
 const createProject = async (req, res) => {
   try {
     const newProject = new Project(req.body);
@@ -19,4 +21,39 @@ const createProject = async (req, res) => {
   }
 };
 
-module.exports = { getProjects, createProject };
+// PUT (update) project by ID   http://localhost
+const updateProject = async (req, res) => {
+  try {
+    const updated = await Project.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // return the updated document
+    );
+    if (!updated) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// DELETE project by ID
+const deleteProject = async (req, res) => {
+  try {
+    const deleted = await Project.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+    res.json({ message: 'Project deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject
+};
--- routes/ProjectRoutes.js
+++ routes/ProjectRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getProjects, createProject } = require('../controllers/projectController');
+const {
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject
+} = require('../controllers/ProjectController');
 
 const router = express.Router();
 
-router.get('/', getProjects);
-router.post('/', createProject);
+router.get('/', getProjects);          // GET all
+router.post('/', createProject);      // POST new
+router.put('/:id', updateProject);    // PUT update by ID
+router.delete('/:id', deleteProject); // DELETE by ID
 
 module.exports = router;
